8f31a35db4cadd8afe76edeb4f22b08544fc50e0
ayoub_ahabchane <mr.ayoub.ahabchane@gmail.com>
2023-02-20 10:36:26 +0100

    Add custom tailwindcss tokens & web fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Inter",
+        serif: "Lora",
+        mono: "Inconsolate",
+      },
+      colors: {
+        primary: {
+          300: "#3A3A3A",
+          400: "#2D2D2D",
+          500: "#1F1F1F",
+          600: "#050505",
+        },
+        secondary: {
+          400: "#757575",
+          300: "#E9E9E9",
+          200: "#F4F4F4",
+        },
+        danger: "#FF5252",
+        accent: "#A445ED",
+      },
+    },
   },
   plugins: [],
 };
