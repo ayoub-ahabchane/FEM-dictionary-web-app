43a53653f7b1eef2cfcc52c8da741f4bcf839d39
ayoub_ahabchane <mr.ayoub.ahabchane@gmail.com>
2023-02-23 13:51:01 +0100

    Added footer

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -17,9 +17,11 @@ function App() {
   }, [themeColor]);
 
   return (
-    <div className="mx-auto flex h-full max-w-3xl flex-col px-6 pt-6 md:px-10 md:pt-14">
+    <div
+      className={`font-${themeFont} mx-auto flex h-full max-w-3xl flex-col px-6 pt-6 md:px-10 md:pt-14`}
+    >
       <Header />
-      <main className={`font-${themeFont} pb-10 pt-44 md:pt-60`}>
+      <main className={`grow pb-10 pt-44 md:pt-60`}>
         <AnimatePresence mode="wait">
           {navigation.state === "loading" ? (
             <motion.p
@@ -37,6 +39,24 @@ function App() {
           )}
         </AnimatePresence>
       </main>
+      <footer className="pb-6 text-center text-sm text-secondary-400">
+        A{" "}
+        <a
+          className="font-bold transition-colors duration-300 focus-within:text-accent hover:text-accent"
+          href="https://www.frontendmentor.io"
+          target="_blank"
+        >
+          Frontend Mentor
+        </a>{" "}
+        challenge by{" "}
+        <a
+          className="font-bold transition-colors duration-300 focus-within:text-accent hover:text-accent"
+          href="https://www.frontendmentor.io/profile/ayoub-ahabchane"
+          target="_blank"
+        >
+          Ayoub Ahabchane
+        </a>
+      </footer>
     </div>
   );
 }
